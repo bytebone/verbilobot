e0320e8af33f592e3dbf710be4fab19131565d3a
bytebone <18621898+bytebone@users.noreply.github.com>
2024-12-11 21:38:59 +0100

    added thread support to admin alerts

--- internal/admin/Alert.go
+++ internal/admin/Alert.go
@@ -3,18 +3,40 @@ package admin
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 
 	"github.com/go-telegram/bot"
 )
 
 func Alert(ctx context.Context, b *bot.Bot, content string) {
-	adminChat := os.Getenv("VERBILO_ADMIN_CHAT")
+	adminChatIDString := os.Getenv("VERBILO_ADMIN_CHAT")
+	if adminChatIDString == "" {
+		return
+	}
+	adminChatID, err := strconv.ParseInt(adminChatIDString, 10, 64)
+	if err != nil {
+		log.Printf("Admin chat ID (\"%s\") has to be a whole number, but is not", adminChatIDString)
+		return
+	}
 
-	b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID: adminChat,
+	params := &bot.SendMessageParams{
+		ChatID: adminChatID,
 		Text:   fmt.Sprintf("⚠ ADMIN ALERT\n\n%s", content),
 		// ParseMode: models.ParseModeMarkdownV1,
-	})
+	}
 
+	if adminThreadIDString := os.Getenv("VERBILO_ADMIN_THREAD_ID"); adminThreadIDString != "" {
+		adminThreadID, err := strconv.ParseInt(adminThreadIDString, 10, 64)
+		if err != nil {
+			log.Printf("Admin thread ID (\"%s\") has to be a whole number, but is not", adminThreadIDString)
+			return
+		}
+		params.MessageThreadID = int(adminThreadID)
+	}
+
+	if _, err = b.SendMessage(ctx, params); err != nil {
+		log.Printf("Could not send message: %v", err)
+	}
 }
