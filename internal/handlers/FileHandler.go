fad94840ab679d04aea8527326caca9c3446b283
bytebone <18621898+bytebone@users.noreply.github.com>
2025-04-17 17:46:50 +0200

    improved error handling for large files

--- internal/handlers/FileHandler.go
+++ internal/handlers/FileHandler.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"strings"
@@ -13,6 +14,10 @@ import (
 	"github.com/go-telegram/bot/models"
 )
 
+var errorFileTooBig = errors.New("file too big")
+var errorFileNoMedia = errors.New("file has no media")
+var errorFileUnsupported = errors.New("file is unsupported")
+
 func FileMatcher(update *models.Update) bool {
 	return update.Message.Video != nil || update.Message.VideoNote != nil || update.Message.Audio != nil || update.Message.Voice != nil || update.Message.Document != nil
 }
@@ -21,39 +26,86 @@ func FileHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	getFile := func() (*models.File, error) {
 		switch {
 		case update.Message.Video != nil:
+			if update.Message.Video.FileSize >= 20000000 {
+				return &models.File{
+					FileSize: update.Message.Document.FileSize,
+					FilePath: update.Message.Document.FileName,
+				}, errorFileTooBig
+			}
 			return b.GetFile(ctx, &bot.GetFileParams{
 				FileID: update.Message.Video.FileID,
 			})
 		case update.Message.VideoNote != nil:
+			if update.Message.VideoNote.FileSize >= 20000000 {
+				return &models.File{
+					FileSize: update.Message.Document.FileSize,
+					FilePath: update.Message.Document.FileName,
+				}, errorFileTooBig
+			}
 			return b.GetFile(ctx, &bot.GetFileParams{
 				FileID: update.Message.VideoNote.FileID,
 			})
 		case update.Message.Audio != nil:
+			if update.Message.Audio.FileSize >= 20000000 {
+				return &models.File{
+					FileSize: update.Message.Document.FileSize,
+					FilePath: update.Message.Document.FileName,
+				}, errorFileTooBig
+			}
 			return b.GetFile(ctx, &bot.GetFileParams{
 				FileID: update.Message.Audio.FileID,
 			})
 		case update.Message.Voice != nil:
+			if update.Message.Voice.FileSize >= 20000000 {
+				return &models.File{
+					FileSize: update.Message.Document.FileSize,
+					FilePath: update.Message.Document.FileName,
+				}, errorFileTooBig
+			}
 			return b.GetFile(ctx, &bot.GetFileParams{
 				FileID: update.Message.Voice.FileID,
 			})
 		case update.Message.Document != nil:
+			if update.Message.Document.FileSize >= 20000000 {
+				return &models.File{
+					FileSize: update.Message.Document.FileSize,
+					FilePath: update.Message.Document.FileName,
+				}, errorFileTooBig
+			}
 			if strings.HasPrefix(update.Message.Document.MimeType, "audio/") || strings.HasPrefix(update.Message.Document.MimeType, "video/") {
 				return b.GetFile(ctx, &bot.GetFileParams{
 					FileID: update.Message.Document.FileID,
 				})
 			} else {
-				log.Println("Denied file of type: " + update.Message.Document.MimeType)
-				return nil, fmt.Errorf("message does not contain an audio file")
+				// og.Println("Denied file of type: " + update.Message.Document.MimeType)
+				return nil, errorFileNoMedia
 			}
 		default:
-			return nil, fmt.Errorf("message does not contain an audio file")
+			// log.Println("Denied unknown message type")
+			return nil, errorFileUnsupported
 		}
 	}
 	f, err := getFile()
-	if err != nil {
+	if errors.Is(err, errorFileTooBig) {
+		log.Println("Denied file over 20 MB")
+		admin.Alert(ctx, b, fmt.Sprintf("Telegram Error: File > 20 MB received\nFilename: %s\nFilesize: %d MB\nUser: @%s", f.FilePath, f.FileSize/1000000, update.Message.From.Username))
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: update.Message.Chat.ID,
-			Text:   "I can only process audio files. Please try again with an audio file.",
+			Text:   "Due to Telegram limitations, the maximum filesize I can process is 20 Megabytes.",
+		})
+		return
+	} else if errors.Is(err, errorFileNoMedia) {
+		log.Println("Denied file of type: " + update.Message.Document.MimeType)
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text:   "This doesn't seem to be an audio or video that I can process.",
+		})
+		return
+	} else if errors.Is(err, errorFileUnsupported) {
+		log.Println("Denied file of unsupported type")
+		b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text:   "This message type is not supported.",
 		})
 		return
 	} else {
@@ -104,7 +156,11 @@ func FileHandler(ctx context.Context, b *bot.Bot, update *models.Update) {
 	text, err := fileutils.Transcribe(ctx, transcodedFile)
 	if err != nil {
 		log.Println("Error: ", err)
-		admin.Alert(ctx, b, fmt.Sprintf("Transcription error: %v", err))
+		fi, err := transcodedFile.Stat()
+		if err != nil {
+			log.Println("Error: ", err)
+		}
+		admin.Alert(ctx, b, fmt.Sprintf("Transcription error: %v\nFilename: %s\nFilesize: %d MB\nUser: @%s", err, rawFile.Name(), fi.Size()/1000000, update.Message.From.Username))
 		b.SendMessage(ctx, &bot.SendMessageParams{
 			ChatID: update.Message.Chat.ID,
 			Text:   "I encountered an unknown issue during transcription. Please try again later.",
